26ee74443fdbf8e5121f3162c8088685e98fe330
andy <andymaker156@gmail.com>
2021-10-19 10:57:13 -0400

    fix dup

--- generate/genBody.py
+++ generate/genBody.py
@@ -9,6 +9,7 @@ str6='</span><span style="font-size:12px;">&nbsp;&nbsp;&nbsp;(as of '
 str7=')</span> </p>   <ul class="list-inline">    <li class="mr-none" title="" data-toggle="tooltip" data-original-title="Source"> <a rel="nofollow noopener" href="www.amazon.com" target="_blank">  <i class="fa fa-code"></i> <span style="font-size:10px;">From Amazon.com</span> </a> </li>  </ul>  </td>  <td width="10"></td> </tr> </tbody>  </table> </div>'
 
 text=''
+url_list=[]
 with open("amz.csv", "r") as infile, open("body.html", "w") as outfile:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(infile)
@@ -20,9 +21,11 @@ with open("amz.csv", "r") as infile, open("body.html", "w") as outfile:
         # only show those has discount
         if (float(row[3]) != 0):
             url = row[4] + '?tag=udeals0d-20&linkCode=ogi&th=1'
-            #print(row[2], row[3])
-            price = '<span style="color:red;">' + row[2] + "</span>  (was $<strike>" + row[3] + "</strike>)"
-            text = str1 + url + str2 + row[5] + str3 + url + str4 + row[1] + str5 + price + str6 + row[7] + str7
-            outfile.write(text)
+            if url not in url_list:
+                url_list.append(url)
+                #print (url_list.index(url))
+                price = '<span style="color:red;">' + row[2] + "</span>  (was $<strike>" + row[3] + "</strike>)"
+                text = str1 + url + str2 + row[5] + str3 + url + str4 + row[1] + str5 + price + str6 + row[7] + str7
+                outfile.write(text)
 infile.close()
 outfile.close()
